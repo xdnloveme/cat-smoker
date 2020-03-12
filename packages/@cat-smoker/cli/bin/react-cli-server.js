9607e8272c33dc5c22360b55cf997026cd18f92b
Tanghy <leon@TanghydeMacBook-Pro.local>
2020-03-12 16:35:46 +0800

    chore(add lerna)

---
+++ packages/@cat-smoker/cli/bin/react-cli-server.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+const program = require('commander')
+
+program
+  .version(require('../package').version)
+
+program
+  .usage('<command> [options]')
+  .command('create <app-name>')
+  .description('create a new react project')
+  .action((name, cmd) => {
+    console.log('指令', name, cmd);
+  })
+
+program
+  .command('add', 'add module to exist project')
+  .command('delete', 'delete a module from project')
+  .command('list', 'list all the modules')
+
+// 解析命令行参数
+program.parse(process.argv);
